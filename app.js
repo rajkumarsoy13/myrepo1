b222516c6e5c4fca280bcc55a661302d80698780
Administrator <rajkumar.soy@mobiloittegroup.com>
2016-05-03 14:38:56 +0530

    changes made

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(4000, function(){
+  console.log('listening on *:4000');
 });
 
 /*var express = require("express");
